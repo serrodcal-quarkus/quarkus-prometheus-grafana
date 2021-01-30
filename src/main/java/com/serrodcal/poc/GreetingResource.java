01e7d8bfeee7f26f0234658a15d6b50cf7970bc1
SERGIO RODRIGUEZ CALVO <SERGIO.RODRIGUEZ.CALVO@ing.com>
2021-01-30 19:27:15 +0100

    added metrics to hello resource

--- src/main/java/com/serrodcal/poc/GreetingResource.java
+++ src/main/java/com/serrodcal/poc/GreetingResource.java
@@ -3,6 +3,9 @@ package com.serrodcal.poc;
 import io.quarkus.vertx.web.Route;
 import io.smallrye.mutiny.Uni;
 import io.vertx.core.http.HttpMethod;
+import org.eclipse.microprofile.metrics.MetricUnits;
+import org.eclipse.microprofile.metrics.annotation.Counted;
+import org.eclipse.microprofile.metrics.annotation.Timed;
 
 import javax.inject.Singleton;
 
@@ -10,6 +13,8 @@ import javax.inject.Singleton;
 public class GreetingResource {
 
     @Route(path = "/hello", methods = HttpMethod.GET)
+    @Counted(name = "performedChecks", description = "How many primality checks have been performed.")
+    @Timed(name = "checksTimer", description = "A measure of how long it takes to perform the primality test.", unit = MetricUnits.MILLISECONDS)
     public Uni<String> hello() {
         return Uni.createFrom().item("Hello!");
     }
